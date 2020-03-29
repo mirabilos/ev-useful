a1aa3a339c66b00d5acae87348d9d97a81bb69a1
mirabilos <t.glaser@tarent.de>
2020-03-30 01:13:53 +0200

    handle pg_proc.proisagg/prokind
    drop aggregates first (they depend on other functions)
    then drop remaining routines with correct type
    
    tested in 9.6.6 (jessie) and 12.2 (sid), window functions untested

--- SQL/except.sql
+++ SQL/except.sql
@@ -1,4 +1,4 @@
--- Copyright © 2019
+-- Copyright © 2019, 2020
 --	mirabilos <t.glaser@tarent.de>
 --
 -- Provided that these terms and disclaimer and all copyright notices
@@ -23,6 +23,7 @@
 
 DO $$
 DECLARE
+	q TEXT;
 	r RECORD;
 BEGIN
 	-- triggers
@@ -110,13 +111,44 @@ BEGIN
 		EXECUTE format('DROP SEQUENCE %I.%I;',
 		    r.nspname, r.relname);
 	END LOOP;
-	-- functions / procedures
+	-- aggregate functions first (because they depend on other functions)
 	FOR r IN (SELECT pns.nspname, pp.proname, pp.oid
-		FROM pg_catalog.pg_proc pp, pg_catalog.pg_namespace pns
+		FROM pg_catalog.pg_proc pp, pg_catalog.pg_namespace pns, pg_catalog.pg_aggregate pagg
 		WHERE pns.oid=pp.pronamespace
 		    AND pns.nspname NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
+		    AND pagg.aggfnoid=pp.oid
 	    ) LOOP
-		EXECUTE format('DROP FUNCTION %I.%I(%s);',
+		EXECUTE format('DROP AGGREGATE %I.%I(%s);',
+		    r.nspname, r.proname,
+		    pg_get_function_identity_arguments(r.oid));
+	END LOOP;
+	-- routines (functions, aggregate functions, procedures, window functions)
+	IF EXISTS (SELECT * FROM pg_catalog.pg_attribute
+		WHERE attrelid='pg_catalog.pg_proc'::regclass
+		    AND attname='prokind' -- PostgreSQL 11+
+	    ) THEN
+		q := 'CASE pp.prokind
+			WHEN ''p'' THEN ''PROCEDURE''
+			WHEN ''a'' THEN ''AGGREGATE''
+			ELSE ''FUNCTION''
+		    END';
+	ELSIF EXISTS (SELECT * FROM pg_catalog.pg_attribute
+		WHERE attrelid='pg_catalog.pg_proc'::regclass
+		    AND attname='proisagg' -- PostgreSQL ≤10
+	    ) THEN
+		q := 'CASE pp.proisagg
+			WHEN true THEN ''AGGREGATE''
+			ELSE ''FUNCTION''
+		    END';
+	ELSE
+		q := '''FUNCTION''';
+	END IF;
+	FOR r IN EXECUTE 'SELECT pns.nspname, pp.proname, pp.oid, ' || q || ' AS pt
+		FROM pg_catalog.pg_proc pp, pg_catalog.pg_namespace pns
+		WHERE pns.oid=pp.pronamespace
+		    AND pns.nspname NOT IN (''information_schema'', ''pg_catalog'', ''pg_toast'')
+	    ' LOOP
+		EXECUTE format('DROP %s %I.%I(%s);', r.pt,
 		    r.nspname, r.proname,
 		    pg_get_function_identity_arguments(r.oid));
 	END LOOP;
