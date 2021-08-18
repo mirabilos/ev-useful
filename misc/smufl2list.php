ae6fa247d6f729d2a8ae106f616079119b52f796
mirabilos <m@mirbsd.org>
2021-08-19 00:03:25 +0200

    first useful version to work with SMuFL git head (commit 170cc21360bd27a663871bd87de24bda542f4906, tag v1.4 + docfix)

--- misc/smufl2list.php
+++ misc/smufl2list.php
@@ -21,114 +21,166 @@
  * Parse SMuFL metadata and output wtf(1) lines.
  */
 
-require_once('/home/tglase/Misc/Vendor/hello-php-world/common/minijson.php');
+// https://github.com/tarent/hello-php-world/blob/master/common/minijson.php
+require_once(dirname(__FILE__) . '/minijson.php');
 
-function bla($msg, $extra=false) {
-	if ($extra !== false)
-		$msg .= ': ' . minijson_encode($extra);
-	fwrite(STDERR, 'E: ' . $msg . "\n");
-	exit(1);
-}
-
+/* return value, error handling functions */
 $rv = 0;
-function blub($msg, $extra=false) {
+function warnx($msg, $extra=false) {
 	global $rv;
 	if ($extra !== false)
 		$msg .= ': ' . minijson_encode($extra);
 	fwrite(STDERR, 'E: ' . $msg . "\n");
-	$rv = 1;
+	if (!$rv)
+		$rv = 1;
 }
-function chk() {
+function checkAccumulatedErrors() {
 	global $rv;
 	if ($rv)
 		exit($rv);
 }
+function errx($msg, $extra=false) {
+	warnx($msg, $extra);
+	checkAccumulatedErrors();
+}
 
-$Icls = file_get_contents('/tmp/smufl/metadata/classes.json');
+/* slurp files; cwd must be git@github.com:w3c/smufl checkout */
+$Icls = file_get_contents('metadata/classes.json');
 if ($Icls === false)
-	bla("file_get_contents('/tmp/smufl/metadata/classes.json')");
-$Inam = file_get_contents('/tmp/smufl/metadata/glyphnames.json');
+	errx("file_get_contents('metadata/classes.json')");
+$Inam = file_get_contents('metadata/glyphnames.json');
 if ($Inam === false)
-	bla("file_get_contents('/tmp/smufl/metadata/glyphnames.json')");
+	errx("file_get_contents('metadata/glyphnames.json')");
 
+/* decode files */
 if (!minijson_decode($Icls, $icls))
-	bla('minijson_decode classes', array(
+	errx('minijson_decode classes', array(
 		'input' => $Icls,
 		'message' => $icls,
 	    ));
 if (!minijson_decode($Inam, $inam))
-	bla('minijson_decode names', array(
+	errx('minijson_decode names', array(
 		'input' => $Inam,
 		'message' => $inam,
 	    ));
 
+/* generate output classes: glyphname ⇒ list (later sorted) of classes */
 $ocls = array();
-foreach (array_keys($icls) as $ncls) {
-	foreach ($icls[$ncls] as $nam) {
+foreach (array_keys($icls) as $Ncls) {
+	if (!($ncls = strval($Ncls)))
+		warnx('class?', $Ncls);
+	foreach ($icls[$ncls] as $Nam) {
+		if (!($nam = strval($Nam)))
+			warnx('class member?', array($ncls => $Nam));
 		if (!isset($ocls[$nam]))
 			$ocls[$nam] = array();
 		$ocls[$nam][] = $ncls;
 	}
 }
-chk();
+/* and sort them */
 foreach (array_keys($ocls) as $nam) {
 	if (!sort($ocls[$nam], SORT_STRING))
-		bla('sort', $ocls[$nam]);
+		warnx('sort', $ocls[$nam]);
 }
+checkAccumulatedErrors();
 
-function norm($n, $cp) {
+/* convert {,U+,U-}nnn to UCS codepoint standard notation */
+function normaliseCodepoint($n, $cn) {
 	global $inam;
 
-	$c = preg_replace('/^[Uu][+-]/', '', $cp);
-	if (!$c)
-		return '';
+	/* absence of (alternate) codepoint? */
+	if (!isset($inam[$n][$cn]))
+		return ('');
+	$cp = strval($inam[$n][$cn]);
+	/* remove præfix if present (it was not in a very old revision) */
+	if (($c = preg_replace('/^[Uu][+-]/', '', $cp)) === null)
+		errx('preg_replace UCS', $cp);
+	/* ensure hex number */
 	if (!preg_match('/^0*[0-9a-fA-F]{1,5}$/', $c))
-		bla('preg_match', array(
+		errx('preg_match UCS', array(
 			'Name' => $n,
 			'input' => $inam[$n],
 			'p' => $c,
-		));
+		    ));
+	/* parse hex number */
 	$n = hexdec($c);
-	if ($n < 1 || $n > 0x10FFFF)
-		bla('hexdec', array(
+	/* type and range check; we deliberately disallow U+0000 */
+	if (!is_int($n) || $n < 1 || $n > 0x10FFFF)
+		errx('hexdec', array(
 			'Name' => $n,
 			'input' => $inam[$n],
 			'pin' => $c,
 			'pnum' => $n,
-		));
-	return sprintf(($n < 0x10000) ? 'U+%04X' : 'U-%08X', $n);
+		    ));
+	/* render as UCS codepoint standard notation */
+	return (sprintf(($n < 0x10000) ? 'U+%04X' : 'U-%08X', $n));
 }
 
-$hit = array();
-foreach (array_keys($inam) as $nam) {
-	$tag = '[SMuFL]';
-	if (!isset($ocls[$nam])) {
-		$tag .= ' [SMuFL:classless]';
-	} else {
-		foreach ($ocls[$nam] as $ncls)
-			$tag .= sprintf(' [SMuFL:class:%s]', $ncls);
-	}
-	$desc = $nam . ' “' . $inam[$nam]['description'] . '”';
-	$code = norm($nam, $inam[$nam]['codepoint']);
-	$alt = norm($nam, isset($inam[$nam]['alternateCodepoint']) ? $inam[$nam]['alternateCodepoint'] : '');
-	if ($alt) {
-		if (isset($hit[$alt]))
-			blub('duplicate', array(
-				'alt' => $alt,
-				't1' => $hit[$alt],
-				't2' => array($nam => $inam[$nam]),
-			));
+/* possibly emit link at regular codepoint position */
+function process_alternateCodepoint($nam, $code, $alt, $body) {
+	global $hit;
+	global $inam;
+
+	/* skip alt: https://github.com/w3c/smufl/issues/195 */
+	if ($nam === 'mensuralBlackSemibrevisVoid' && $alt === 'U-0001D1B9')
+		return;
+
+	/* duplicate check */
+	if (isset($hit[$alt]))
+		warnx('duplicate', array(
+			'alt' => $alt,
+			't1' => $hit[$alt],
+			't2' => array($nam => $inam[$nam]),
+		    ));
+	else
 		$hit[$alt] = array($nam => $inam[$nam]);
-		printf("%s\t%s %s (cf. %s)\n", $alt, $tag, $desc, $code);
-		$desc .= ' (cf. ' . $alt . ')';
+	/* output line */
+	printf("%s\t%s [SMuFL:alternateCodepoint] (cf. %s)\n",
+	    $alt, $body, $code);
+}
+
+/* duplicate codepoint check */
+$hit = array();
+/* process all glyphnames */
+foreach (array_keys($inam) as $Nam) {
+	if (!($nam = strval($Nam)))
+		warnx('name?', $Nam);
+	if (!isset($inam[$nam]['description']))
+		$desc = '';
+	else
+		$desc = strval($inam[$nam]['description']);
+	/* fix stray spaces in historic data */
+	if (($desc = preg_replace("/[ \n\r\t\v]+/", ' ', $desc)) === null)
+		errx('preg_replace desc', array($nam => $inam[$nam]));
+	if (!($desc = trim($desc)))
+		warnx('missing description', array($nam => $inam[$nam]));
+	/* collect wtf RHS */
+	$body = '';
+	/* tag (omit just [SMuFL] since they all have at least one) */
+	if (!isset($ocls[$nam]))
+		$body .= '[SMuFL:classless] ';
+	else
+		foreach ($ocls[$nam] as $ncls)
+			$body .= sprintf('[SMuFL:class:%s] ', $ncls);
+	/* glyphname and (quoted) description */
+	$body .= $nam . ' “' . $desc . '”';
+	/* codepoint and alternate */
+	if (!($code = normaliseCodepoint($nam, 'codepoint')))
+		errx('no codepoint', array($nam => $inam[$nam]));
+	if (($alt = normaliseCodepoint($nam, 'alternateCodepoint'))) {
+		process_alternateCodepoint($nam, $code, $alt, $body);
+		$body .= ' (cf. ' . $alt . ')';
 	}
+	/* duplicate check */
 	if (isset($hit[$code]))
-		blub('duplicate', array(
+		warnx('duplicate', array(
 			'code' => $code,
 			't1' => $hit[$code],
 			't2' => array($nam => $inam[$nam]),
-		));
-	$hit[$code] = array($nam => $inam[$nam]);
-	printf("%s\t%s %s\n", $code, $tag, $desc);
+		    ));
+	else
+		$hit[$code] = array($nam => $inam[$nam]);
+	/* and emit main wtf line */
+	printf("%s\t%s\n", $code, $body);
 }
-chk();
+checkAccumulatedErrors();
